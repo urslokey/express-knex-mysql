5a3a18ef688ba8734377cefa978a9baf4ab2c116
lokey2094 <50478024+lokey2094@users.noreply.github.com>
2022-08-25 16:28:59 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,27 +1,25 @@
-/* eslint-disable import/order */
 const express = require('express');
-const dbConnection = require('./knex').development;
-const knex = require('knex')(dbConnection);
-
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const port = 3001;
 
-// eslint-disable-next-line no-unused-vars
-app.get('/', (req, res) => {
-  knex.select('*').from('employees').then((data) => {
-    res.send(data);
-  });
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/',(req,res)=>{
+    res.send(`<h2 style="text-align:center;padding-top:20%;">It's Working ...</h2>`);
+
+    
+})
+
+
+app.get('/items',(req,res)=>{
+    res.json({ status:1,data:[{ name:"Apple" },{ name:"Mango" },{ name:"Grape" }] });
+})
+
+
+app.listen(port,()=>{
+    console.log(`Running at ${port}`);
 });
-
-app.get('/where', (req, res) => {
-  knex.select('*').from('transactions').where({ id: 1 }).then((data) => {
-    res.send(data);
-  });
-});
-
-app.get('/join', (req, res) => {
-  knex.select('*').from('employees').leftJoin('transactions', 'transactions.employee_id', 'employees.id').then((data) => {
-    res.send(data);
-  });
-});
-
-app.listen(3000);
